994ba75a09bb63f5b4cb7ecf17e1a848b490471c
Marc <marcplustwo@gmail.com>
2023-04-16 17:20:08 -0300

    better backup logging

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
+echo Start update $(date)
+
 rsync -e "ssh -o StrictHostKeyChecking=no -i /secrets/.ssh/id_ed25519 -p23" \
   -aAXH \
   --info=progress2 \
   --delete \
   /data $BACKUP_TARGET
+
+echo Finish update $(date)
